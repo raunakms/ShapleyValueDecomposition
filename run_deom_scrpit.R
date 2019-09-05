86277fe9f4df9d450b625996c4e56e271fdbed95
raunakms <sraunak@gmail.com>
2019-09-05 17:09:04 +0545

    update source function

--- run_deom_scrpit.R
+++ run_deom_scrpit.R
@@ -3,9 +3,11 @@ dir.wrk <- getwd()
 
 ### DEFINE FILE ---
 file.dat <- file.path(dir.wrk, "dummy_observations.tsv")
+file.function <- file.path(dir.wrk, "ShapleyValueDecomposition.R")
 
 ### LOAD DATA ---
 dat <- read.delim(file.dat, header=TRUE, stringsAsFactors=FALSE)
 
 ### RUN SHAPLEY VALUE DECOMPOSITON ---
+source(file.function)
 list.df <- ShapleyValue.Decomposition(dat)
